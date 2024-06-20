bc9934bdd2abbeec03e8c96932a797e3c81d7ecf
Eduardo Guevara <edugvra@outlook.com>
2024-06-19 20:07:12 -0500

    token

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import axios from "axios";
 import useStore from "../useStore.js";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+  const navigate = useNavigate();
   const token = useStore((state) => state.token);
 
   function logout() {
     axios.get("https://perfectosri.software-total.com/api/v1/logout/");
     localStorage.removeItem("token");
+    useStore.getState().clearUser();
+    navigate("/");
+
+
   }
 
   return (
@@ -15,6 +21,7 @@ function Dashboard() {
       <h1>Dashboard</h1>
       {/* mostrar el token */}
         <p>{token}</p>
+        <button onClick={logout}>Logout</button>
     </div>
   )
 }
